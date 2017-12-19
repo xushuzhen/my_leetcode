fbb4ae0dea0739aef6b54561b36df26df7c77fad
许树真 <shuzhen.xu@zebra-c.com>
2017-12-19 17:28:29 +0800

    4

--- 4_Median_of_Two_Sorted_Arrays.py
+++ 4_Median_of_Two_Sorted_Arrays.py
@@ -29,6 +29,48 @@ class Solution(object):
         else:
             return 0.0
 
+    def findMedianSortedArrays2(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums1.extend(nums2)
+        nums1.sort()
+        l = len(nums1)
+        if l == 0:
+            return 0.0
+        m = l % 2
+        a = l / 2
+        if m > 0:
+            return float(nums1[a])
+        else:
+            return float(nums1[a] + nums1[a - 1]) / 2
+
+    def findMedianSortedArrays3(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        all_nums = nums1 + nums2
+        len_all = len(all_nums)
+        all_nums.sort()
+
+        if len_all == 0:
+            return None
+        elif len_all == 1:
+            return all_nums[0]
+        elif len_all == 2:
+            return ((all_nums[0] + all_nums[1]) / 2.0)
+
+        if len_all % 2 == 0:
+            median = (all_nums[(len(all_nums) / 2)] + all_nums[(len(all_nums) / 2) - 1]) / 2.0
+        else:
+            median = all_nums[(len(all_nums) / 2)]
+
+        return median
+
 
 if __name__ == '__main__':
     solution = Solution()
@@ -38,6 +80,9 @@ if __name__ == '__main__':
     n4 = []
     n5 = [1]
     n6 = [2, 3]
-    print solution.findMedianSortedArrays(n1, n2)
-    print solution.findMedianSortedArrays(n3, n4)
-    print solution.findMedianSortedArrays(n5, n6)
+    # print solution.findMedianSortedArrays2(n1, n2)
+    # print solution.findMedianSortedArrays2(n3, n4)
+    # print solution.findMedianSortedArrays2(n5, n6)
+    # print solution.findMedianSortedArrays2(n4, n4)
+    print solution.findMedianSortedArrays2(n4, n5)
+    print solution.findMedianSortedArrays3(n4, n5)
